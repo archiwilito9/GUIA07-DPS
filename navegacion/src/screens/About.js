e6c66face9d9adecfd5dcedf27cdea3385f6ce26
Marvin Tobar <archiwilito9@yahoo.com>
2021-10-23 23:58:53 -0600

    arreglado

--- navegacion/src/screens/About.js
+++ navegacion/src/screens/About.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {View,Text} from 'react-native';
-export default function About(){
- return(
- <View>
- <Text> Estamos en About</Text>
- </View>
- );
+import { View, Text } from 'react-native';
+export default function About() {
+    return (
+        <View>
+            <Text> Estamos en About</Text>
+        </View>
+    );
 }
--- navegacion/src/screens/Contact.js
+++ navegacion/src/screens/Contact.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {View,Text} from 'react-native';
-export default function Contact(){
- return(
- <View>
- <Text> Estamos en Contact </Text>
- </View>
- );
+import { View, Text } from 'react-native';
+export default function Contact() {
+    return (
+        <View>
+            <Text> Estamos en Contact </Text>
+        </View>
+    );
 }
